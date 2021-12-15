8fc48b347c3be03b659086c71eedc44bf5a48329
Leandro de Brito Alencar <zzleandrobritozz@gmail.com>
2021-12-14 21:31:15 -0300

    First Component finished

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,7 +2,18 @@ import * as S from './styles'
 
 const Main = () => (
   <S.Wrapper>
-    <h1>React Avançado</h1>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
   </S.Wrapper>
 )
 
