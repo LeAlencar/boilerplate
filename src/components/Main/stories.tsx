689d9b565583f066d4e2e01021d3513df69c9d73
Leandro de Brito Alencar <zzleandrobritozz@gmail.com>
2021-12-17 20:41:58 -0300

    Using storybook essentials

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,14 +1,16 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Meta, Story } from '@storybook/react/types-6-0'
 
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main title={text('Title', 'React Avançado')} />
-)
+export const Basic: Story = (args) => <Main {...args} />
+
+export const Default: Story = (args) => <Main {...args} />
+Default.args = {
+  title: 'Teste',
+  description: 'teste'
+}
